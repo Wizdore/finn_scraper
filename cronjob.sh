cc69aff958ff8f7ee0f056a03be414236b1108ed
Wizdore <shada.kaalo@gmail.com>
2020-12-30 07:32:58 +0100

    Cleaning up file structure

--- cronjob.sh
+++ cronjob.sh
@@ -3,4 +3,4 @@
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 PIPFILE=$script_dir/Pipfile
 export PIPENV_PIPFILE=$(cd $(dirname $PIPFILE) && pwd)/$(basename $PIPFILE)
-pipenv run python $script_dir/app.py
+pipenv run python $script_dir/src/app.py
